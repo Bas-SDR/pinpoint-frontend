5cb62af0daebd3cdfcc8a671945cbfaea2f56358
Bas-SDR <bas_reintsema@hotmail.com>
2025-08-19 18:44:31 +0200

    Removed earlier hardcoded info as it is now mapped.

--- src/pages/newsPage/News.jsx
+++ src/pages/newsPage/News.jsx
@@ -69,39 +69,6 @@ function News() {
             <SponsorBar sponsorLocation="right"/>
         </div>
     );
-
-    // );
-    // <article className="news-article">
-    //     <InfoTile tileTitle={"Seizoen voorbij!"} tileOrientation="left" tileType="text">
-    //         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur dignissimos dolores
-    //             nam nisi optio rem sapiente ullam, vero! Accusamus aliquid culpa cumque cupiditate et hic magnam
-    //             magni odit voluptates!</p>
-    //         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut doloremque eligendi error
-    //             et eveniet, impedit ipsam iure optio quae reiciendis sed sit sunt tempore!</p>
-    //     </InfoTile>
-    //     <InfoTile tileImage={newsArt1} imageAltTile={"News Article 1"} tileOrientation="left" tileType="image"/>
-    // </article>
-    // <article className="news-article">
-    //     <InfoTile tileImage={newsArt2} imageAltTile={"News Article 2"} tileOrientation="right"
-    //               tileType="image"/>
-    //     <InfoTile tileTitle={"Finale dag volgende week"} tileOrientation="right" tileType="text">
-    //         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda, beatae debitis
-    //             deserunt doloremque doloribus dolorum, eum hic iure minus officiis pariatur quaerat, quo ratione
-    //             rem sunt voluptatem? Libero neque nostrum numquam quibusdam quod repellendus, totam voluptatibus
-    //             voluptatum. Labore, obcaecati! </p>
-    //     </InfoTile>
-    // </article>
-    // <article className="news-article">
-    //     <InfoTile tileTitle={"Laatste competitie dagen"} tileOrientation="left" tileType="text">
-    //         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur dignissimos dolores
-    //             nam nisi optio rem sapiente ullam, vero! Accusamus aliquid culpa cumque cupiditate et hic magnam
-    //             magni odit voluptates!</p>
-    //         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut doloremque eligendi error
-    //             et eveniet, impedit ipsam iure optio quae reiciendis sed sit sunt tempore!</p>
-    //     </InfoTile>
-    //     <InfoTile tileImage={newsArt3} imageAltTile={"News Article 3"} tileOrientation="left" tileType="image"/>
-    // </article>
-
 }
 
 export default News;
