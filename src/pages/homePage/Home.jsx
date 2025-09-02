05a5151aadf05e4f565c03bae3a530890597a91e
Bas-SDR <bas_reintsema@hotmail.com>
2025-09-02 18:47:10 +0200

    Added homepage, renamed NewsTile to InfoTile so it can be reused on other places. Added additional clean-view formatting.

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,13 +1,25 @@
 import './Home.css';
-import BigCard from "../../components/bigCard/BigCard.jsx";
 import Header from "../../components/header/Header.jsx";
+import SponsorBar from "../../components/sponsorBar/SponsorBar.jsx";
+import stockphoto from "../../../src/assets/stockphoto.jpg"
+import NewsTile from "../../components/newsTile/NewsTile.jsx";
 
 function Home() {
     return (
         <div className="outer-container-incl-sponsor">
             <Header>Home</Header>
-            <h2>Welkom op de website van Bowlingvereniging XXYYZZ</h2>
-
+            <div className="page-content homepage-styling">
+                <NewsTile
+                    tileImage={stockphoto}
+                    imageAltTile="Stock photo"
+                    tileTitle="Welkom"
+                    tileType="clean-view"
+                >
+                    <p>Hier vind je info over Bowling Vereniging XXYYZZ, en de uitslagen van de gespeelde leagues en toernooien</p>
+                </NewsTile>
+            </div>
+            <SponsorBar sponsorLocation="left"/>
+            <SponsorBar sponsorLocation="right"/>
         </div>
     );
 }
