5ebbbd2fd72ed50f7b4b9a8bc527e0e55fd5cb8a
Bas-SDR <bas_reintsema@hotmail.com>
2025-09-02 16:18:14 +0200

    Added small changes to main page and sponsorbar css.

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -3,13 +3,13 @@ import BigCard from "../../components/bigCard/BigCard.jsx";
 
 function Home() {
     return (
-        <>
+        <div className="outer-container-incl-sponsor">
             <h1>Dit is de home pagina</h1>
             <h2>Dit is h2</h2>
             <h3>Dit is h3</h3>
             <h4>Dit is h4</h4>
             <p>Dit is p</p>
-        </>
+        </div>
     );
 }
 
