e9ed8ed29f0b5e71f0545f6d709bcaf6fa0c7c21
Bas-SDR <bas_reintsema@hotmail.com>
2026-01-07 16:27:00 +0100

    Added leave team function for players, added kick player for captain and admins. Changed some english words to Dutch

--- src/pages/teamPage/Team.jsx
+++ src/pages/teamPage/Team.jsx
@@ -1,6 +1,6 @@
 import './Team.css';
 import Header from "../../components/header/Header.jsx";
-import React from "react";
+import React, {useContext, useState} from "react";
 import useProfileData from "../../hooks/useProfileData.js";
 import {useParams} from "react-router-dom";
 import StatusMessage from "../../components/statusMessage/StatusMessage.jsx";
@@ -8,11 +8,54 @@ import SmallCard from "../../components/smallCard/SmallCard.jsx";
 import BigCard from "../../components/bigCard/BigCard.jsx";
 import getCurrentEntity from "../../helpers/getCurrentPageId.js";
 import noLogo from '../../assets/nologo.png';
+import {AuthContext} from "../../context/AuthContext.jsx";
+import axios from "axios";
+import Button from "../../components/button/Button.jsx";
 
 function Team() {
     const {teams, players, loading, error} = useProfileData();
     const {teamId} = useParams();
+    const {userId, roles} = useContext(AuthContext);
+
+    const [errorState, setError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
+
+    const token = localStorage.getItem("token");
     const currentTeam = getCurrentEntity(teams, teamId, "id");
+
+    async function kickPlayer(player) {
+        setError(false);
+        setErrorMsg("");
+        setSuccessMsg("");
+        try {
+            await axios.patch(
+                `http://localhost:8080/teams/${currentTeam.id}/kick/${player.id}`,
+                {},
+                {headers: {Authorization: `Bearer ${token}`}}
+            );
+            setSuccessMsg(`${player.firstName} ${player.lastName} is succesvol verwijderd.`);
+        } catch (e) {
+            console.error(e);
+            setError(true);
+            setErrorMsg("Kick mislukt.");
+        }
+    }
+
+    async function leaveTeam(player) {
+        try {
+            await axios.patch(
+                `http://localhost:8080/teams/${currentTeam.id}/leave/${player.id}`,
+                {},
+                {headers: {Authorization: `Bearer ${token}`}}
+            );
+            setSuccessMsg(`Je hebt succesvol het team verlaten.`);
+        } catch (e) {
+            console.error(e);
+            setError(true);
+            setErrorMsg("Team verlaten mislukt.");
+        }
+    }
 
     return (
         <div className="outer-container-excl-sponsor">
@@ -31,7 +74,8 @@ function Team() {
                 />
             }
             <h2>Spelers</h2>
-
+            {successMsg && <p className="success-message">{successMsg}</p>}
+            {errorState && <p className="error-message">{errorMsg}</p>}
             <div className="team-collection">
                 {currentTeam ? (
                     players
@@ -42,7 +86,7 @@ function Team() {
                                     type="management"
                                     userId={player.id}
                                     userName={`${player.firstName} ${player.lastName}`}
-                                    userFunction="Member"
+                                    userFunction={currentTeam?.captainId === player.id ? "Captain" : "Speler"}
                                     userEmail="To follow"
                                 />
                                 <SmallCard
@@ -50,10 +94,25 @@ function Team() {
                                     highestGame={player.stats.highestGame}
                                     highestSeries={player.stats.highestSeries}
                                 />
+                                {player.id === userId && (
+                                        <Button
+                                            onClick={() => leaveTeam(player)}
+                                        >
+                                            Verlaat team
+                                        </Button>
+                                )}
+
+                                {(roles.includes("ROLE_ADMIN") || currentTeam.captainId === userId) && (
+                                        <Button
+                                            onClick={() => kickPlayer(player)}
+                                        >
+                                            Kick speler
+                                        </Button>
+                                )}
                             </div>
                         ))
                 ) : (
-                    <h2>No teams have been found</h2>
+                    <h2>Geen teams gevonden</h2>
                 )}
             </div>
         </div>
